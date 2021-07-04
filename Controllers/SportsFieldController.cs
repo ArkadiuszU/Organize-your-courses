1d94d3a23b55fd6c0d7633ee5f329b48a1465a5b
Arek U <arkadiuszurbankiewicz@gmail.com>
2021-07-04 03:26:02 +0200

    change concering with swich of project conception

--- Controllers/SportsFieldController.cs
+++ Controllers/SportsFieldController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using OrganizeYourCourses.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +9,71 @@ namespace OrganizeYourCourses.Controllers
 {
     public class SportsFieldController : Controller
     {
+        public static List<SportField> sportFields = new List<SportField>
+        {
+            new SportField
+            {
+                Type = SportFieldType.football,
+                Name = "Orlik",
+                Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
+                Features = new SportFieldFeature
+                {
+                    Lighting = true,
+                    Cloakroom = true,
+                    ArtificalTurf = false,
+                    EquipmentRental = true
+                },
+                City = "Poznań",
+                Street = "Wojskowa",
+                Number = "9g",
+                PhoneNumber = 601901564,
+                Email = "sample@sample.com"
+            },
+            new SportField
+            {
+                Type = SportFieldType.basketball,
+                Name = "Wielofunkcyjne",
+                Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
+                Features = new SportFieldFeature
+                {
+                    Lighting = true,
+                    Cloakroom = true,
+                    ArtificalTurf = true,
+                    EquipmentRental = false
+                },
+                City = "Poznań",
+                Street = "Wojskowa",
+                Number = "15",
+                PhoneNumber = 601901564,
+                Email = "sample@sample.com"
+            },
+            new SportField
+            {
+                Type = SportFieldType.volleyball,
+                Name = "Plazowe",
+                Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
+                Features = new SportFieldFeature
+                {
+                    Lighting = false,
+                    Cloakroom = false,
+                    ArtificalTurf = false,
+                    EquipmentRental = true
+                },
+                City = "Poznań",
+                Street = "Świętego Wawrzyńca",
+                Number = "3b",
+                PhoneNumber = 601901564,
+                Email = "sample@sample.com"
+            },
+    };
+        
         public IActionResult Index()
         {
-            return View();
+            return View(sportFields);
+        }
+        public IActionResult Details()
+        {
+            return View(sportFields[2]);
         }
     }
 }
---
+++ Models/SportField.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrganizeYourCourses.Models
+{
+    public enum SportFieldType { football, basketball, volleyball}
+    public class SportFieldFeature
+    {
+        public bool Lighting { get; set; }
+        public bool Cloakroom { get; set; }
+        public bool ArtificalTurf { get; set; }
+        public bool EquipmentRental { get; set; }
+    }
+    public class SportField
+    {
+        public SportFieldType Type { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public SportFieldFeature Features { get; set; }
+        public string City{ get; set; }
+        public string Street{ get; set; }
+        public string Number { get; set; }
+        public int PhoneNumber { get; set; }
+        public string Email { get; set; }
+    }
+}
